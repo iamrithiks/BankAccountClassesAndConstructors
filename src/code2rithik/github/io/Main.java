f292b27e156e9c8e3080f482c0f095562c7c6197
Rithik Samanthula <rithikdeveloper754@gmail.com>
2021-10-07 19:38:25 +0530

    Initial Commit

---
+++ src/code2rithik/github/io/Main.java
@@ -0,0 +1,50 @@
+package code2rithik.github.io;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create a new class for a bank account
+        // Create fields for the account number, balance, customer name, email and phone number.
+        // Create getters and setters for each field.
+        // Create two additional methods
+        // 1. To allow the customer to deposit funds (this should increment the balance field.)
+        // 2. To allow the customer to withdraw funds. This should deduct from the balance field,
+        // but not allow the withdrawal to complete if there are insufficient funds.
+        // You will want to create various code in the Main Class (the one created by IntelliJ) to
+        // confirm your code is still working
+        // Add some System.out.println's in the two methods above as well.
+
+        // Part 1 Below
+
+//        Account bobsAccount = new Account(); // "12345", 0.00, "Bob Brown", "myemail@bob.com",
+//                // "(087) 123-456");
+//        System.out.println(bobsAccount.getNumber());
+//        System.out.println(bobsAccount.getAccountBalance());
+//
+//        bobsAccount.deposit(50.0);
+//        bobsAccount.withdrawal(100.0);
+//
+//        bobsAccount.deposit(51.0);
+//        bobsAccount.withdrawal(100.0);
+//
+//        Account ricksAccount = new Account("Rick", "rick@email.com", "12345");
+//        System.out.println(ricksAccount.getNumber() + " name " + ricksAccount.getCustomerName());
+//        System.out.println("Current balance is " + ricksAccount.getAccountBalance());
+//        ricksAccount.withdrawal(100.55);
+
+//        Part 2 Below
+
+//        VipPerson person1 = new VipPerson();
+//        System.out.println(person1.getName());
+//
+//        VipPerson person2 = new VipPerson("Bob", 2500.00);
+//        System.out.println(person2.getName());
+//
+//        VipPerson person3 = new VipPerson("Rick", 100.00, "rick@email.com");
+//        System.out.println(person3.getName());
+//        System.out.println(person3.getEmailAddress());
+//        System.out.println(person3.getCreditLimit());
+
+    }
+}
